0719f47782ce04458d6397997cc778ebf6afa3f2
Olamilekan <adebukolaolamilekan123@gmail.com>
2024-02-10 23:52:05 -1100

    updating

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lato: "Lato", sans-serif;
+      }
+    },
   },
   plugins: [],
 }
